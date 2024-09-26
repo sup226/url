297652aede993fb7d812d298f47dd449ecd34b01
sup226 <rlaalswl1102@naver.com>
2024-09-26 11:35:56 +0900

    Servlet만으로 요청과 응답 처리

--- src/main/java/com/spring/basic/BasicApplication.java
+++ src/main/java/com/spring/basic/BasicApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
+// 서블릿으로 사용할 클래스를 자동 스캔.
 @ServletComponentScan
 @SpringBootApplication
 public class BasicApplication {
--- src/main/java/com/spring/basic/servlet/web/MemberJoinServlet.java
+++ src/main/java/com/spring/basic/servlet/web/MemberJoinServlet.java
@@ -1,8 +1,10 @@
 package com.spring.basic.servlet.web;
 
+import com.spring.basic.servlet.domain.Member;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
@@ -10,9 +12,11 @@ import java.io.PrintWriter;
 
 @WebServlet("/servlet/member/join")
 public class MemberJoinServlet extends HttpServlet {
+
     @Override
-    protected void service(HtppServletRequest request,
+    protected void service(HttpServletRequest request,
                            HttpServletResponse response) throws ServletException, IOException {
+
         System.out.println("MemberJoinServlet의 service 호출!");
         String id = request.getParameter("id");
         String pw = request.getParameter("pw");
@@ -26,22 +30,22 @@ public class MemberJoinServlet extends HttpServlet {
         response.setCharacterEncoding("UTF-8");
         PrintWriter w = response.getWriter();
 
-        w.write("<!DOCTYPE html>
-<html lang="ko">
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-</head>
-<body>
-    <h2>회원 가입 성공!</h2>
-    <ul>
-        <li>" +member.getUserName() + "님 환영합니다!</li>\n" +
-        <li>id: ???</li>
-        <li>나이: ???</li>
-    </ul>
-</body>
-</html>");
+        w.write("<!DOCTYPE html>\n" +
+                "<html lang=\"ko\">\n" +
+                "<head>\n" +
+                "    <meta charset=\"UTF-8\">\n" +
+                "    <title>Document</title>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "    <h2>회원 가입 성공!</h2>\n" +
+                "    <ul>\n" +
+                "        <li>" + member.getUserName() + "님 환영합니다!</li>\n" +
+                "        <li>id: " + member.getId() + "</li>\n" +
+                "        <li>나이: " + member.getAge() + "세</li>\n" +
+                "    </ul>\n" +
+                "</body>\n" +
+                "</html>");
+
+
     }
-
-
 }
